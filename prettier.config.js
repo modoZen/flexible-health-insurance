b9bb20686230d6aa368fe99c5017a89d81fd6ea7
Max Diaz <mdiazn@uni.pe>
2025-07-25 11:28:35 -0500

    chore: add and configure Prettier

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+};
+
+export default prettierConfig;
