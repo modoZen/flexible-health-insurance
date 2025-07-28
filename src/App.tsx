8b3b1686c0f46d03d0834d423060330b7c1de1fa
Max Diaz <mdiazn@uni.pe>
2025-07-27 23:33:47 -0500

    feat: add Loader component and show it in App on loading

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,24 @@ import { NotFoundPage } from './pages/NotFound/NotFoundPage';
 import { PlansPage } from './pages/plans/PlansPage';
 import { PrivateRoute } from './router/PrivateRoute';
 import { useAppSelector } from './store';
+import { Loader } from './components/Loader/Loader';
 
 function App() {
   const { user } = useAppSelector((state) => state.user);
+  const isLoading = useAppSelector((state) => state.ui.loading);
   const isAuthenticated = !!user;
 
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route element={<PrivateRoute isAuthenticated={isAuthenticated} />}>
-        <Route path='/plans' element={<PlansPage />} />
-      </Route>
-      <Route path='*' element={<NotFoundPage />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route element={<PrivateRoute isAuthenticated={isAuthenticated} />}>
+          <Route path='/plans' element={<PlansPage />} />
+        </Route>
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+      {isLoading && <Loader />}
+    </div>
   );
 }
 
